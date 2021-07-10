03dcbeed25c996a6c02c08105cfe7feea3d10b69
Mariya <maria.k070416@gmail.com>
2021-07-10 13:25:08 +0300

    создала калькулятор

---
+++ scr.js
@@ -0,0 +1,27 @@
+function plus() {
+    var num1, num2, result;
+
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(nam1);
+
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(nam2);
+
+    result = num1 + num2;
+
+    document.getElementById('out').innerHTML = result;
+}
+
+function minus() {
+    var num1, num2, result;
+
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(nam1);
+
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(nam2);
+
+    result = num1 - num2;
+
+    document.getElementById('out').innerHTML = result;
+} 
